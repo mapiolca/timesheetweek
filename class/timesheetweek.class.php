0f7d2b7c76ff0b15d29ceade5390cad0ae0f69ba
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-10-07 23:11:38 +0200

    Retirer entity de l'insertion element_time

--- class/timesheetweek.class.php
+++ class/timesheetweek.class.php
@@ -536,9 +536,8 @@ class TimesheetWeek extends CommonObject
 
                         $taskTimestamp = $this->normalizeLineDate($line->day_date);
                         $sql = "INSERT INTO ".MAIN_DB_PREFIX."element_time(";
-                        $sql .= " entity, fk_user, fk_task, elementtype, fk_element, duration, task_date, import_key";
+                        $sql .= " fk_user, fk_task, elementtype, fk_element, duration, task_date, import_key";
                         $sql .= ") VALUES (";
-                        $sql .= " ".(!empty($this->entity) ? (int) $this->entity : 1).",";
                         $sql .= " ".(!empty($this->fk_user) ? (int) $this->fk_user : "NULL").",";
                         $sql .= " ".(!empty($line->fk_task) ? (int) $line->fk_task : "NULL").",";
                         $sql .= " 'task',";
