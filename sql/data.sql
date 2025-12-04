1df17ecee2af2dc118d7eec6d371d14fd75ba08c
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-12-04 21:33:49 +0100

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -4,6 +4,6 @@ INSERT INTO llx_c_email_templates (entity,module,type_template,lang,private,fk_u
         'Rappel du vendredi soir',
         100,
         1,
-        0,
+        NULL,
         "Rappel Feuilles d\'heures hebodmadaires",
         "Bonjour,<br /> Merci de soumettre vos feuilles d\'heures de la semaine pour lundi matin 8h.<br />Bon week-end.");
