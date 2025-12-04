d87ff5c9f8a887066082a85f0ebec504a8468e0a
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-12-04 21:22:49 +0100

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO llx_c_email_templates (entity,module,type_template,lang,private,fk_user,datec,label,position,active,joinfiles,topic,content)
     VALUES (
-        1,'timesheetweek','timesheetweek_reminder','fr_FR', 0,NULL, NOW(),
+        0,'timesheetweek','actioncomm_send','fr_FR', 0,NULL, NOW(),
         'Rappel du vendredi soir',
-        1,1,1,
+        100,'isModEnabled("timesheetweek")',0,
         "Rappel Feuilles d\'heures hebodmadaires",
         "Bonjour,<br /> Merci de soumettre vos feuilles d\'heures de la semaine pour lundi matin 8h.<br />Bon week-end.");
